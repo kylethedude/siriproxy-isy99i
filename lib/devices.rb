d87657de1dc2c4103c405a2955c293269f657311
Kyle <kylethedood@gmail.com>
2013-02-03 12:17:19 -0800

    Complete rewrite for adding a simple interface

--- lib/devices.rb
+++ lib/devices.rb
@@ -1,76 +1,309 @@
-require 'cora'
+require 'rubygems'
+require 'httparty'
+require 'singleton'
 
-
-  def deviceCrossReference(deviceName)
-
-
-    if (deviceName.match(/christmas tree|christmas lights|tree/i))
-
-      @dimmable = 0 #must be set to 1 in order to recognize dimmable devices
-                    #otherwise, not necessary or set to 0.
-
-    return "19%207D%20DB%201" #can be set to either a scene (#####) or a device (##%20##%20##%20#)
-    #return 24409             #if set to a device, the %20 must be used in place of the space and
-                              #you must use quotation marks around it ex. return "12%2034%2056%207"
-                              #NOTE: If any section of your device address has a leading zero in it,
-                              # it must be left off from the settings i.e. 1A.0B.9F = 1A %20 B %20 9F %20 1
-
-
-    elsif (deviceName.match(/kitchen lights|kitchen light/i))
-    return "19%2027%2073%201"
-
-
-    elsif (deviceName.match(/night scene|night/i))
-    return 12393
-
-
-    elsif (deviceName.match(/dining room|dining room light/i))
-      @dimmable = 1
-    return "1A%2028%20BB%201"
-
-
-    elsif (deviceName.match(/test|text|test switch|text switch/i))
-      @dimmable = 1
-    return "1A%2020%20BA%201"   #device = test dimmer
-    #return "1B%2041%2082%201"  #device = test relay
-    #return 27377               #scene for both
-
-
-    elsif (deviceName.match(/.*undercounter light.*|.*under counter light.*|.*under cabinet light.*|.*cabinet light.*|.*counter light.*/i))
-    return 28826
-
-
-    elsif (deviceName.match(/thermostat|temperature/i))
-    return "14%2013%2064%201"
-
-
-    elsif (deviceName.match(/garage door light/i))
-      @dimmable = 1
-    return "1A%2024%2061%201"
-
-
-    elsif (deviceName.match(/.*garage lights|.*(lights)*.(garage)/i))
-      @dimmable = 0
-    return "1B%2040%2069%201"
-
-
-    elsif (deviceName.match(/.*front porch light|.*porch light/i))
-      @dimmable = 1
-    return "1A%202A%20B6%201"
-
-
-    elsif (deviceName.match(/.*entry light|.*entry/i))
-      @dimmable = 1
-    return "1A%20B%209F%201"
-
-
-    else 
-    return 0
+class Devices
+  # Index Constants
+  DEVICENAME = 0
+  DEVICEID = 1
+  DIMMABLE = 2
+ 
+  def initialize(d)
+    # A multdimensional array where each element is an array consisting of 4 elements:
+    #     0 => Device Name: A regular expression of the device name to be recongnized by Siri
+    #     1 => Device Address: Either the ISY-99i Scene ID or the Device ID with spaces converted to %20
+    #     2 => Is the Device Dimmable? 0 = no, 1 = yes
+    if (d.nil?)
+        @devices = []
+    else
+        @devices = d
     end
-    return deviceName
   end
+ 
+  def add(deviceName, deviceId, isDimmable)
+    @devices << [deviceName, deviceId, isDimmable]
+  end
+  
+  # Returns a Device object based upon the Device Name or nil if no Device could be found
+  def findDevice(deviceName)
+    puts "findDevice(#{deviceName})"
+    @devices.each_index do |i|
+        if (@devices[i][DEVICENAME].match(/#{deviceName}/i))
+            return Device.new(@devices[i][DEVICENAME], @devices[i][DEVICEID], @devices[i][DIMMABLE])
+        end
+    end
+    
+    return nil
+  end
+  
+  # Returns a Device object based upon the index it was added to the class, starting with index 0. Returns nil if an invalid index is provided.
+  #  => Use this for quick access to a device
+  def getDevice(index)
+    if (index >= 0 && index < devices.length)
+        return Device.new(@devices[index][DEVICENAME], @devices[i][DEVICEID], @devices[i][DIMMABLE])
+    else
+        return nil
+    end
+  end
+end
 
+class Rest    
+    attr_accessor :host, :auth, :elkCode
+    
+    include Singleton
+    include HTTParty
+    format :xml
+    
+    def setConnParameters(host, user, pass)
+        @host = host
+        @auth = {:username => "#{user}", :password => "#{pass}"}  
+    end
+    
+    def setElkParameters(elkcode)
+        @elkCode = elkcode
+    end
+end
 
-    #elsif(deviceName.match(/undercounter lights/i) || deviceName.match(/under counter lights/i) || deviceName.match(/under cabinet lights/i) || deviceName.match(/cabinet lights/i) || deviceName.match(/counter lights/i))
-    #return 28826
+class Device
+    attr_reader :deviceAddress
+    
+    def initialize(deviceName, deviceAddress, isDimmable)
+        @deviceName = deviceName
+        @deviceAddress = deviceAddress
+        @isDimmable = isDimmable
+        
+        if (is_number?(deviceAddress))
+            @isScene = 1
+            @isDimmable = 0
+        else
+            @isScene = 0
+        end
+    end
+    
+    # Returns a Boolean value representing if the Device is Dimmable
+    def isDimmable()
+        if (@isDimmable == 1)
+            return true
+        end
+    
+        return false
+    end
+  
+    # Returns a Boolean value representing if the Device is a Scene
+    def isScene()
+        if (@isScene == 1)
+            return true
+        end
+    
+        return false
+    end
+    
+    # Returns the %/On/Off status of the Device [string]
+    def getStatus
+        # Scenes don't have a status
+        if (@isScene == 1)  
+            return ""
+        else
+            return Rest.get("#{Rest.instance.host}/rest/status/#{@deviceAddress}", :basic_auth => Rest.instance.auth).parsed_response["properties"]["property"]["formatted"]
+        end
+    end
+    
+    # Probe the device to determine if it's capable of dimming; true => capable, false => not capable
+    def determineIfDimmable
+        uom = Rest.get("#{Rest.instance.host}/rest/status/#{@deviceAddress}", :basic_auth => Rest.instance.auth).parsed_response["properties"]["property"]["uom"]
+        
+        if (uom.match(/^%/))
+            return true
+        end
+        
+        return false
+    end
+    
+    # Returns the On/Off status of the Device [string]
+    def getOnOffStatus
+        # Scenes don't have a status
+        if (@isScene == 1)  
+            return ""
+        else
+            status = getStatus();
+            onLevel = status.to_i
 
+            if (onLevel > 0)
+                return "On"
+            else
+                return status
+            end
+        end
+    end
+    
+    # Returns the percentage value of the On Level; -1 on error [integer]
+    def getOnLevel
+        # Scenes don't have a status
+        if (@isScene == 1)  
+            return -1
+        else
+            onlevel = getStatus()
+            puts "getOnLevel => #{onlevel}"
+            
+            if (onlevel == "On") 
+                return 100
+            elsif (onLevel == "Off")
+                return 0
+            else 
+                return onlevel.to_i  
+            end  
+        end
+    end
+    
+    # Set Device On Level percentage [string]
+    def setOnLevel(percent)
+        percent = percent.to_i
+        if (percent >= 0 && percent <= 100)
+            percent *= 2.55
+            nodeCmd("DON/#{percent.to_i}")
+        end
+    end
+    
+    def turnOff
+        nodeCmd("DOF")
+    end
+    
+    def turnOn
+        nodeCmd("DON")
+    end
+    
+    def turnFastOff
+        nodeCmd("DFOF")
+    end
+    
+    def turnFastOn
+        nodeCmd("DFON")
+    end
+    
+    # Increases the On Level by ~3%
+    def brighten
+        nodeCmd("BRT")
+    end
+    
+    # Decreases the On Level by ~3%
+    def dim
+        nodeCmd("DIM")
+    end
+    
+    def nodeCmd(cmd)
+        return Rest.get("#{Rest.instance.host}/rest/nodes/#{@deviceAddress}/cmd/#{cmd}", :basic_auth => Rest.instance.auth)    
+    end
+    
+    def is_number?(n)
+        true if Float(n) rescue false
+    end
+end
+
+# Elk Classes are still under development
+=begin
+class ElkArea
+    attr_reader :areaId  
+    
+    def initialize(area)
+        @areaId = area
+    end  
+    
+    def getStatus
+        return Rest.get("#{Rest.instance.host}/rest/elk/area/#{@areaId}/get/status", :basic_auth => Rest.instance.auth).parsed_response["properties"]["property"]["formatted"]    
+    end
+    
+    def armAway
+        areaCmdWithCode("arm?armType=1&")
+    end
+    
+    def armStay
+        areaCmdWithCode("arm?armType=2&")
+    end
+    
+    def armStayInstant
+        areaCmdWithCode("arm?armType=3&")
+    end
+    
+    def armNight
+        areaCmdWithCode("arm?armType=4&")
+    end
+    
+    def armNightInstant
+        areaCmdWithCode("arm?armType=5&")
+    end
+    
+    def armVacation
+        areaCmdWithCode("arm?armType=6&")
+    end
+    
+    def disarm
+        areaCmdWithCode("disarm?")
+    end
+    
+    def areaCmd(cmd)
+        return Rest.get("#{Rest.instance.host}/rest/elk/area/#{@areaId}/cmd/#{cmd}", :basic_auth => Rest.instance.auth)    
+    end
+    
+    def areaCmdWithCode(cmd)
+        return areaCmd("#{cmd}code=#{Rest.instance.elkCode}")
+    end
+end
+
+class ElkZones  
+    def initialize
+        @zones = []
+        
+        getTopology()
+    end  
+    
+    def add(zoneId, zoneName)
+        @zones << [zoneId, zoneName]   
+    end
+    
+    def getTopology
+        topology = getStatus()
+        
+        # Iterate through topology and add zones  
+    end
+    
+    def getStatus
+        return Rest.get("#{Rest.instance.host}/rest/elk/get/status", :basic_auth => Rest.instance.auth)    
+    end
+    
+    def armAway
+        areaCmdWithCode("arm?armType=1&")
+    end
+    
+    def armStay
+        areaCmdWithCode("arm?armType=2&")
+    end
+    
+    def armStayInstant
+        areaCmdWithCode("arm?armType=3&")
+    end
+    
+    def armNight
+        areaCmdWithCode("arm?armType=4&")
+    end
+    
+    def armNightInstant
+        areaCmdWithCode("arm?armType=5&")
+    end
+    
+    def armVacation
+        areaCmdWithCode("arm?armType=6&")
+    end
+    
+    def disarm
+        areaCmdWithCode("disarm?")
+    end
+    
+    def areaCmd(cmd)
+        return Rest.get("#{Rest.instance.host}/rest/elk/area/#{@areaId}/cmd/#{cmd}", :basic_auth => Rest.instance.auth)    
+    end
+    
+    def areaCmdWithCode(cmd)
+        return areaCmd("#{cmd}code=#{Rest.instance.elkCode}")
+    end
+end
+=end
